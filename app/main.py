1c475856c972862faed74e5a8fe6da76a5042988
hryhorskyi <martin.hryhorskyi@gmail.com>
2024-04-19 16:36:41 +0300

    https

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 from app.api import attendance, student, university_class
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
+import uvicorn
 
 app = FastAPI()
 
 app.include_router(attendance.router, prefix="/attendance")
 app.include_router(student.router, prefix="/students")
 app.include_router(university_class.router, prefix="/classes")
+
+app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        ssl_keyfile="/path/to/your/ssl/key.pem",
+        ssl_certfile="/path/to/your/ssl/cert.pem",
+    )
